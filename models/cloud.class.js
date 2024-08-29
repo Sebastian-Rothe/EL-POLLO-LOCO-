fd930d4cad34eaaca6556aa78bbcd4bf60469c17
Sebastian-Rothe <sebi1995@gmx.at>
2024-08-29 12:46:32 +0200

    first commit

---
+++ models/cloud.class.js
@@ -0,0 +1,13 @@
+class Cloud extends MovableObject{
+    y = 25;
+    width = 500;
+    height = 250;
+
+    constructor(){
+        super().loadImage('./img/5_background/layers/4_clouds/1.png');
+
+        this.x = 0 + Math.random() * 500;
+       
+
+    };
+}
