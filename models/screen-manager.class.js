5feba0b50f70a4a71c74e5e0d2d68068b8b80c5a
Sebastian-Rothe <sebi1995@gmx.at>
2024-09-12 18:06:53 +0200

    extra start btn added

--- models/screen-manager.class.js
+++ models/screen-manager.class.js
@@ -27,169 +27,12 @@ class ScreenManager {
     this.clearScreen();
     this.drawImage("./img/9_intro_outro_screens/start/startscreen_1.png");
 
-    this.createStartGameButton();
-    this.createInstructionsButton();
-    this.createImpressumButton();
-  }
-
-  /**
-   * Creates and appends the Start Game button.
-   */
-  createStartGameButton() {
-    this.createButton(
-      "Start Game",
-      () => {
-        this.removeCurrentButtons();
-        this.startGameCallback();
-      },
-      "start"
-    );
-  }
-
-  /**
-   * Creates and appends the Instructions button.
-   */
-  createInstructionsButton() {
-    this.createButton(
-      "Instructions",
-      () => {
-        this.removeCurrentButtons();
-        this.showInstructions();
-      },
-      "instructions"
-    );
-  }
-
-  /**
-   * Creates and appends the Impressum button.
-   */
-  createImpressumButton() {
-    this.createButton(
-      "Impressum",
-      () => {
-        this.removeCurrentButtons();
-        this.showImpressum();
-      },
-      "impressum"
-    );
-  }
-
-  /**
-   * Displays the instructions screen with controls for the game.
-   */
-  showInstructions() {
-    this.clearScreen();
-    this.hideVolumeButtons();
-    const instructionsContent = this.createInstructionsContent();
-    const goBackButton = this.createGoBackButton(instructionsContent);
-
-    document.getElementById("fullscreen").appendChild(instructionsContent);
-    document.getElementById("fullscreen").appendChild(goBackButton);
-  }
-
-  /**
-   * Creates the content for the instructions screen.
-   * @returns {HTMLElement} The instructions content element.
-   */
-  createInstructionsContent() {
-    const instructionsContent = document.createElement("div");
-    instructionsContent.classList.add("impressum-content");
-    instructionsContent.innerHTML = `
-      <span class="impressum-headline">Instructions</span>
-      <p>Move Left: Arrow Left (←) or A</p>
-      <p>Move Right: Arrow Right (→) or D</p>
-      <p>Jump: Spacebar</p>
-      <p>Throw: R</p> 
-  `;
-    return instructionsContent;
-  }
-
-  /**
-   * Creates the go back button for the instructions screen.
-   * @param {HTMLElement} instructionsContent The instructions content element.
-   * @returns {HTMLElement} The go back button element.
-   */
-  createGoBackButton(instructionsContent) {
-    const goBackButton = document.createElement("button");
-    goBackButton.innerText = "Go Back";
-    goBackButton.classList.add("start-button");
-    goBackButton.onclick = () => {
-      instructionsContent.remove();
-      goBackButton.remove();
-      this.showStartScreen();
-    };
-    return goBackButton;
-  }
-
-  /**
-   * Displays the impressum screen with contact information.
-   */
-  showImpressum() {
-    this.clearScreen();
-    this.hideVolumeButtons();
-    const impressumContent = this.createImpressumContent();
-    const goBackButton = this.createGoBackButton(impressumContent);
-    document.getElementById("fullscreen").appendChild(impressumContent);
-    document.getElementById("fullscreen").appendChild(goBackButton);
-  }
-
-  /**
-   * Hides volume control buttons.
-   */
-  hideVolumeButtons() {
-    document.getElementById("button-volume-up").style.display = "none";
-    document.getElementById("button-volume-down").style.display = "none";
-    document.getElementById("button-volume-off").style.display = "none";
-  }
-
-  /**
-   * Creates the impressum content with contact information.
-   * @returns {HTMLElement} The impressum content element.
-   */
-  createImpressumContent() {
-    const impressumContent = document.createElement("div");
-    impressumContent.classList.add("impressum-content");
-    impressumContent.innerHTML = `
-      <span class="impressum-headline">Impressum</span>
-      <span>Sebastian Rothe</span>
-      <span>Mühlwehrstraße, 8</span>
-      <span>70488 Frankfurt Am Main</span>
-      <br>
-      <span>Kontakt</span>
-      <span>Telefon: 0176 52968778</span>
-      <span>E-Mail: sebi1995@gmx.at</span>
-      <br>
-      <span> Erstellt von impressum-generator.info - powered by abfindungshero.de </span>
-      <span>Angaben gemäß Par. 5 DDG</span>
-  `;
-    return impressumContent;
-  }
-
-  /**
-   * Creates a 'Go Back' button and assigns the click handler.
-   * @param {HTMLElement} impressumContent - The impressum content element to be removed on button click.
-   * @returns {HTMLElement} The 'Go Back' button element.
-   */
-  createGoBackButton(impressumContent) {
-    const goBackButton = document.createElement("button");
-    goBackButton.innerText = "Go Back";
-    goBackButton.classList.add("start-button");
-    goBackButton.onclick = () => {
-      this.handleGoBack(impressumContent, goBackButton);
-    };
-    return goBackButton;
-  }
-
-  /**
-   * Handles the actions when the 'Go Back' button is clicked.
-   * @param {HTMLElement} impressumContent - The impressum content element to be removed.
-   * @param {HTMLElement} goBackButton - The 'Go Back' button to be removed.
-   */
-  handleGoBack(impressumContent, goBackButton) {
-    impressumContent.remove();
-    goBackButton.remove();
-    this.showStartScreen();
-    document.getElementById("button-volume-down").style.display = "block";     
+    // Erstellen der Buttons mit der neuen Funktion
+    this.createMultipleButtons([
+      { text: "Start Game", callback: () => { this.removeCurrentButtons(); this.startGameCallback(); }, type: "start" },
+      { text: "Instructions", callback: () => { this.removeCurrentButtons(); this.showInstructions(); }, type: "instructions" },
+      { text: "Impressum", callback: () => { this.removeCurrentButtons(); this.showImpressum(); }, type: "impressum" }
+    ]);
   }
 
   /**
@@ -199,14 +42,12 @@ class ScreenManager {
     this.currentScreen = "gameOver";
     this.clearScreen();
     this.drawImage("./img/9_intro_outro_screens/game_over/oh no you lost!.png");
-    this.createButton(
-      "Play Again",
-      () => {
-        this.removeCurrentButtons();
-        this.restartGameCallback();
-      },
-      "playAgain"
-    );
+
+    // Erstellen der Buttons für "Play Again" und "Return to Start"
+    this.createMultipleButtons([
+      { text: "Play Again", callback: () => { this.removeCurrentButtons(); this.restartGameCallback(); }, type: "playAgain" },
+      { text: "Return to Start", callback: () => { this.removeCurrentButtons(); this.showStartScreen(); }, type: "start-above" }
+    ]);
   }
 
   /**
@@ -216,14 +57,48 @@ class ScreenManager {
     this.currentScreen = "win";
     this.clearScreen();
     this.drawImage("./img/9_intro_outro_screens/win/win_2.png");
-    this.createButton(
-      "Play Again",
-      () => {
-        this.removeCurrentButtons();
-        this.restartGameCallback();
-      },
-      "playAgain"
-    );
+
+    // Erstellen der Buttons für "Play Again" und "Return to Start"
+    this.createMultipleButtons([
+      { text: "Play Again", callback: () => { this.removeCurrentButtons(); this.restartGameCallback(); }, type: "playAgain" },
+      { text: "Return to Start", callback: () => { this.removeCurrentButtons(); this.showStartScreen(); }, type: "start-above" }
+    ]);
+  }
+
+  /**
+   * Centralized function to create multiple buttons.
+   * @param {Array} buttonConfigs - An array of objects containing text, callback, and type for each button.
+   */
+  createMultipleButtons(buttonConfigs) {
+    buttonConfigs.forEach(({ text, callback, type }) => {
+      this.createButton(text, callback, type);
+    });
+  }
+
+  /**
+   * Creates a button and appends it to the screen.
+   * @param {string} text - The text to display on the button.
+   * @param {function} callback - The function to be called when the button is clicked.
+   * @param {string} type - The type of the button (e.g., start, instructions, impressum).
+   */
+  createButton(text, callback, type) {
+    const button = document.createElement("button");
+    button.innerText = text;
+    button.onclick = callback;
+
+    if (type === "start" || type === "playAgain") {
+      button.classList.add("start-button");
+    } else if (type === "start-above") {
+      button.classList.add("start-button-above");
+    } else if (type === "instructions") {
+      button.classList.add("button-start-screen", "instructions");
+    } else if (type === "impressum") {
+      button.classList.add("button-start-screen", "impressum");
+    }
+
+    document.getElementById("fullscreen").appendChild(button);
+    this.currentButtons.push(button); 
+    this.toggleButtonsVisibility();
   }
 
   /**
@@ -247,30 +122,6 @@ class ScreenManager {
   }
 
   /**
-   * Creates a button and appends it to the screen.
-   * @param {string} text - The text to display on the button.
-   * @param {function} callback - The function to be called when the button is clicked.
-   * @param {string} type - The type of the button (e.g., start, instructions, impressum).
-   */
-  createButton(text, callback, type) {
-    const button = document.createElement("button");
-    button.innerText = text;
-    button.onclick = callback;
-
-    if (type === "start" || type === "playAgain") {
-      button.classList.add("start-button");
-    } else if (type === "instructions") {
-      button.classList.add("button-start-screen", "instructions");
-    } else if (type === "impressum") {
-      button.classList.add("button-start-screen", "impressum");
-    }
-
-    document.getElementById("fullscreen").appendChild(button);
-    this.currentButtons.push(button); 
-    this.toggleButtonsVisibility();
-  }
-
-  /**
    * Removes all current buttons from the screen.
    */
   removeCurrentButtons() {
@@ -278,11 +129,11 @@ class ScreenManager {
     this.currentButtons = [];
   }
 
-    /**
+  /**
    * Controls the visibility of buttons based on viewport width.
    * Hides buttons if viewport width is less than 500 pixels.
    */
-   toggleButtonsVisibility() {
+  toggleButtonsVisibility() {
     if (window.innerWidth < 480) {
       this.currentButtons.forEach(button => {
         button.style.display = "none"; 
